e6726608980916c0cd0fb6e43e0bb7720f6dc449
Facu Herrera <facuherrera328@gmail.com>
2023-01-19 22:53:18 -0300

    EntregaFinal+FacundoHerrera

--- js/index.js
+++ js/index.js
@@ -1,54 +1,3 @@
-const baseDeDatos = [
-    {
-        id: 1,
-        nombre: 'SHAMPOO CACHORROS PH CONTROLADO OSSPRET',
-        precio: 800,
-        imagen: 'https://d3ugyf2ht6aenh.cloudfront.net/stores/860/952/products/fase-1-131-f10a7fd82ccabc386516235933185496-1024-1024.jpg'
-    },
-    {
-        id: 2,
-        nombre: 'HUESO CORBATA 4/5',
-        precio: 500,
-        imagen: 'https://d3ugyf2ht6aenh.cloudfront.net/stores/860/952/products/fase-1-881-0c2e13fa0b3892e0f516235936019858-1024-1024.jpg'
-    },
-    {
-        id: 3,
-        nombre: 'COMEDERO DE PLÁSTICO MEDIANO - PETS PLAST',
-        precio: 300,
-        imagen: 'https://d3ugyf2ht6aenh.cloudfront.net/stores/860/952/products/fase-2-41-14ba15d80d9073e74016243078065940-1024-1024.jpg'
-    },
-    {
-        id: 4,
-        nombre: 'COMEDERO DE ACERO INOXIDABLE CHICO',
-        precio: 1250,
-        imagen: 'https://d3ugyf2ht6aenh.cloudfront.net/stores/860/952/products/fase-1-11-bcbe9e670387d0063f16235928025779-1024-1024.jpg'
-    },
-    {
-        id: 5,
-        nombre: 'ROYAL CANIN MEDIUM ADULT 3 KG',
-        precio: 3560,
-        imagen: 'https://d3ugyf2ht6aenh.cloudfront.net/stores/860/952/products/royal-canin-medium-61-d77e1db9933933fd6b16225691572931-1024-1024.jpg'
-    },
-    {
-        id: 6,
-        nombre: 'ROYAL CANIN CANICHE POODLE ADULT 3 KG',
-        precio: 4500,
-        imagen: 'https://d3ugyf2ht6aenh.cloudfront.net/stores/860/952/products/royal-perro-dorado-61-7dc87ffa6a0d156e2316225710534993-1024-1024.jpg'
-    },
-    {
-        id: 7,
-        nombre: 'ROYAL CANIN MAXI ADULT +5A 15 KG',
-        precio: 13500,
-        imagen: 'https://d3ugyf2ht6aenh.cloudfront.net/stores/860/952/products/royal-canin-maxi-21-d24a95ff1a8654d0a416225697035096-1024-1024.jpg'
-    },
-    {
-        id: 8,
-        nombre: 'ROYAL CANIN MINI ADULT 1 KG',
-        precio: 2000,
-        imagen: 'https://d3ugyf2ht6aenh.cloudfront.net/stores/860/952/products/rpyaln-canin-1-perro-51-ca9d983ab941f40e4c16225676085880-1024-1024.jpg'
-    }
-];
-
 const productos = document.getElementById('productos');
 const templateCard = document.getElementById('template-card').content;
 const templateCarrito = document.getElementById('template-carrito').content;
@@ -73,7 +22,7 @@ items.addEventListener('click', e => {
 const renderizarProductos = () => {
     baseDeDatos.forEach(producto => {
         templateCard.querySelector('h6').textContent = producto.nombre;
-        templateCard.querySelector('p').textContent = producto.precio;
+        templateCard.querySelector('p').textContent = '$' + producto.precio;
         templateCard.querySelector('img').setAttribute('src', producto.imagen)
         templateCard.querySelector('.btn').dataset.id = producto.id;
 
@@ -143,7 +92,6 @@ const renderizarFooter = () => {
         footerCarrito.innerHTML = `
         <th scope="row" colspan="5">Carrito vacío - comience a comprar!</th>
         `;
-
         return
     }
 
